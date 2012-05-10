1b25a8f39192f1dc7118b8eda37b5b0b6205823f
fuglsang <volmer@gmail.com>
2012-05-11 00:12:14 +0200

    stuff

--- src/main.cpp
+++ src/main.cpp
@@ -24,50 +24,91 @@ pixellight!
 */
 #define WINW		512
 #define WINH		512
-#define RAYSFRAME	512
+#define RAYSFRAME	100
 
 /*
 	globals
 */
-class Node;
-class Geom;
-//Node *	root;
-Vec2	pos;
+Vec2		v00 = Vec2(-1.0f, -1.0f);	// lower left
+Vec2		v10 = Vec2(1.0f, -1.0f);	// lower right
+Vec2		v01 = Vec2(-1.0f, 1.0f);	// upper left
+Vec2		v11 = Vec2(1.0f, 1.0f);		// upper right
+
+LevelNode *	root;
+Vec2		pos;
 
 /*
 	rayline
 */
 inline bool rayline(Vec2 const & u0, Vec2 const & ud, Vec2 const & v0, Vec2 const & v1, float & ut, float & vt)
 {
-	float vdx	= v1.x-v0.x;
-	float vdy	= v1.y-v0.y;
-	float d		= ud.y*vdx - ud.x*vdy;
+	float const vdx	= v1.x-v0.x;
+	float const vdy	= v1.y-v0.y;
+	float const d	= ud.y*vdx - ud.x*vdy;
 
 	if (d != 0.0f)
 	{
-		float const r	= 1.0f / d;
-		float const a	= r * (v0.y - u0.y);
-		float const b	= r * (u0.x - v0.x);
+		float const r = 1.0f / d;
+		float const a = r * (v0.y - u0.y);
+		float const b = r * (u0.x - v0.x);
 
 		ut	= vdx*a + vdy*b;
 		vt	= ud.x*a + ud.y*b;
 
-		return (ut >= 0.0f && vt >= 0.0f && vt <= 1.0f);
+		return (ut > (0.0f + 1e-7/* f32 epsilon */) && vt >= 0.0f && vt <= 1.0f);
 	}
 	else
 	{
 		return false;
 	}
+}
 
+/*
+	swap
+*/
+template <typename T>
+inline void swap(T & a, T & b)
+{
+	T tmp = a;
+	a = b;
+	b = tmp;
+}
+
+/*
+	rot90
+*/
+inline void rot90(Vec2 & v, int ccwSteps)
+{
+	switch (ccwSteps)
+	{
+	case 1:
+		v.y *= -1.0f;
+		swap(v.x, v.y);
+		break;
+
+	case 2:
+		v.x *= -1.0f;
+		v.y *= -1.0f;
+		break;
+
+	case 3:
+		v.x *= -1.0f;
+		swap(v.x, v.y);
+		break;
+	}
 }
 
 /*
 	trace
 */
-inline void trace(Node * node, Vec2 pos, Vec2 dir, float dMax, float & dOut, Geom * & geom)
+inline void trace(LevelNode * node, Vec2 pos, Vec2 dir, float dMax, float & dOut, Geom * & geom)
 {
-	float d = 0.0f;
-	float t;
+	LevelEdge *	e;
+	Vec2		x;
+	float		ut;	// distance along trace direction
+	float		vt;	// distance along edge
+	float		d = 0.0f;
+	bool		f = false;
 
 	// loop until the maximum distance has been reached, or no more nodes
 	while ((d < dMax) && node != NULL)
@@ -75,14 +116,59 @@ inline void trace(Node * node, Vec2 pos, Vec2 dir, float dMax, float & dOut, Geo
 		// find nearest intersection with local geometry
 		//TODO
 
-		// if unsuccessful, then find intersection with local boundary
-		//TODO
+		// if unsuccessful, then find intersection with node boundary
+		if (!f)
+		{
+			// transfer to edge
+			if (rayline(pos, dir, v00, v10, ut, vt))
+			{
+				e		= node->s;// => south
+				pos.x	= ut*dir.x;
+				pos.y	= -1.0f;
+			}
+			else if (rayline(pos, dir, v10, v11, ut, vt))
+			{
+				e		= node->e;// => east
+				pos.x	= 1.0f;
+				pos.y	= ut*dir.y;
+			}
+			else if (rayline(pos, dir, v11, v01, ut, vt))
+			{
+				e		= node->n;// => north
+				pos.x	= ut*dir.x;
+				pos.y	= 1.0f;
+			}
+			else if (rayline(pos, dir, v01, v00, ut, vt))
+			{
+				e		= node->w;// => west
+				pos.x	= -1.0f;
+				pos.y	= ut*dir.y;
+			}
+
+			// transform position
+			rot90(pos, e->ccwSteps);
+
+			if (e->ccwSteps % 2 == 0)
+			{
+				pos.x *= -1.0f;
+			}
+			else
+			{
+				pos.y *= -1.0f;
+			}
+
+			// transform direction
+			rot90(dir, e->ccwSteps);
+
+			// move to connecting node
+			node = e->Node;
+		}
 
 		// move to next node
 		//TODO
 
 		// increment distance travelled
-		d += t;
+		d += ut;
 	}
 
 	// cap the output
@@ -134,15 +220,19 @@ void game()
 */
 int main(int argc, char * argv[])
 {
+	// init
 	glfwInit();
 	glfwSwapInterval(1);// vsync
 	glfwOpenWindowHint(GLFW_WINDOW_NO_RESIZE, GL_TRUE);
 	glfwOpenWindow(WINW, WINH, 8, 8, 8, 0, 0, 0, GLFW_WINDOW);
 	glfwSetWindowTitle("pixellight");
 
+	// loop
 	game();
 
+	// nuke
 	glfwCloseWindow();
 
+	// done
 	return 0;
 }
