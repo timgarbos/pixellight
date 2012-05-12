3a9241625e83cd24eaf7ad0d8fda1dbbcf224d32
Peter Bruun-Rasmussen <p.bruunrasmussen@gmail.com>
2012-05-12 13:58:37 +0200

    changed window title

--- src/main.cpp
+++ src/main.cpp
@@ -1027,7 +1027,7 @@ int main(int argc, char * argv[])
 	glfwEnable(GLFW_STICKY_KEYS);
 	glfwOpenWindowHint(GLFW_WINDOW_NO_RESIZE, GL_TRUE);
 	glfwOpenWindow(WINW, WINH, 8, 8, 8, 0, 0, 0, GLFW_WINDOW);
-	glfwSetWindowTitle("pixellight");
+	glfwSetWindowTitle("Photon Boy");
     
     audioManager = new AudioManager();
     
